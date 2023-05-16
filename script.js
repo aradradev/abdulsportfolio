ea496371427b47c163de2fca9e1bea8a1c9732b4
Abdoul <abdoulramanediallo44@gmail.com>
2023-05-16 10:29:19 +0000

    new update

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const closeBtn = document.getElementById('close-btn');
 hambuger.addEventListener('click', () => {
   hambuger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  closeBtn.classList.toggle("active");
+  closeBtn.classList.toggle('active');
 });
