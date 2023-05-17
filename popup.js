1fb48f4f4f9f12d44531882e40ec07657f37731b
Abdoul <abdoulramanediallo44@gmail.com>
2023-05-17 10:56:45 +0000

    POpup window for desktop version

--- popup.js
+++ popup.js
@@ -2,6 +2,19 @@ const openModalButton = document.querySelectorAll('[data-modal-target]');
 const closeModalButton = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
+// const seeProject = document.querySelector('.btn-project');
+// const modal1 = document.querySelector('body');
+
+// const dataObj = [{
+//   projectId: '1',
+//   title: 'Multi-Post Stories',
+//   technologies: ['HTML', 'Boostrap', 'Ruby on Rails'],
+//   featuredImage: 'images/Snapshoot Portfolio.png',
+//   description: 'Lorem ipsum dolor sit amet',
+//   linkLiveVersion: '#',
+//   linkSource: '#',
+// }];
+
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add('active');
