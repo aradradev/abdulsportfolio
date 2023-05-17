a0140bb180079df3d3990ce8098f341fffdbb030
Abdoul <abdoulramanediallo44@gmail.com>
2023-05-17 13:22:17 +0000

    Object data first changes

--- popup.js
+++ popup.js
@@ -2,18 +2,45 @@ const openModalButton = document.querySelectorAll('[data-modal-target]');
 const closeModalButton = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-// const seeProject = document.querySelector('.btn-project');
-// const modal1 = document.querySelector('body');
+const seeProject1 = document.querySelector('#project-1');
 
-// const dataObj = [{
-//   projectId: '1',
-//   title: 'Multi-Post Stories',
-//   technologies: ['HTML', 'Boostrap', 'Ruby on Rails'],
-//   featuredImage: 'images/Snapshoot Portfolio.png',
-//   description: 'Lorem ipsum dolor sit amet',
-//   linkLiveVersion: '#',
-//   linkSource: '#',
-// }];
+const dataObj = [{
+  projectId: '1',
+  title: 'Profesional Art Printing Data',
+  technologies: ['HTML', 'Boostrap', 'Ruby on Rails'],
+  featuredImage: 'images/Snapshoot Portfolio.png',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+  linkLiveVersion: '#',
+  linkSource: '#',
+}];
+
+seeProject1.addEventListener('click', () => {
+  const titleEl = document.querySelector('.txt-modal');
+  const techEl = document.querySelector('.row-skills ul');
+  const descEl = document.querySelector('.description-modal');
+  const liveEl = document.querySelector('.live');
+  const sourceEl = document.querySelector('.source');
+
+  // Clear out any existing content
+  titleEl.innerHTML = '';
+  techEl.innerHTML = '';
+  descEl.innerHTML = '';
+  liveEl.innerHTML = '';
+  sourceEl.innerHTML = '';
+
+  // Populate the modal with the dataObj content
+  titleEl.innerHTML = dataObj[0].title;
+
+  dataObj[0].technologies.forEach((tech) => {
+    const techItem = document.createElement('li');
+    techItem.innerHTML = tech;
+    techEl.appendChild(techItem);
+  });
+
+  descEl.innerHTML = dataObj[0].description;
+  liveEl.setAttribute('href', dataObj[0].linkLiveVersion);
+  sourceEl.setAttribute('href', dataObj[0].linkSource);
+});
 
 function openModal(modal) {
   if (modal == null) return;
